7ee961532414a4ee496742a225b29890f7ab6ba7
yjocho61 <53847339+yjocho61@users.noreply.github.com>
2019-11-07 19:36:43 +0800

    Add files via upload

--- scanning.py
+++ scanning.py
@@ -1,24 +1,25 @@
 import csv
 
 
+def Scan_print(id, book_info, exist = False):
+    if row[2] == id:
+        print(row[4], '\n', row[5], '\n', row[6])
+        f.write('\n')
+        f.write(row[2])
+        exist = True
+    return exist
+
+
 while True:
-    c = 0
-    name = input('')
+    exist = False  #initialize the existence of the book
+    book_id = input('')
     with open('save.csv', 'a', encoding = 'utf-8-sig') as f:
         with open('nmsbooklist.csv', newline='', encoding = 'utf-8-sig') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
             for row in spamreader:
-                try:
-                    if row[2] == name:
-                        #print(row)
-                        print(row[4], '\n', row[5], '\n', row[6])
-                        f.write('\n')
-                        f.write(row[2])
-                        c += 1
-                except IndexError:
-                  print(row)
-            if c == 0:
+                exist = Scan_print(book_id, row)
+                if exist == True:
+                    break
+            if exist == 0:
                 print('DOES NOT EXIST')
-            elif c > 1:
-                print('MORE THAN ONE BOOKS')
     print('===================================================')
